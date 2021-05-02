12139a85e2749602d901f210246e03c9b93662d5
maker2413 <maker2413@gmail.com>
2021-05-02 01:39:02 -0700

    Writing up sample program

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// Name of the game we will play
+var game string
+
+// Array of our 5 tables
+var tablelist [5]int
+
+// Slice for our deck
+var dark []string
+
 func main() {
 	cards := []string{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spade")
@@ -9,6 +18,15 @@ func main() {
 	for index, card := range cards {
 		fmt.Println(index, card)
 	}
+
+	game="BlackJack"
+	fmt.Println(game)
+
+	for i := range tablelist {
+		tablelist[i]=i + 1
+	}
+
+	fmt.Println(tablelist)
 }
 
 func newCard() string {

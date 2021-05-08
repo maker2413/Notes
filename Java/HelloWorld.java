96b28d0e6f8fa1a79590e481c8d9de87930fa6d8
maker2413 <maker2413@gmail.com>
2021-05-07 23:33:23 -0700

    added more notes for Java and fixed typo in spinnaker

---
+++ Java/HelloWorld.java
@@ -0,0 +1,12 @@
+// This is a single line comment
+
+/* This is a block of comments
+Three
+Two
+One*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}

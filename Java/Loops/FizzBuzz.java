14088ab244e9172f4c82af5e90943176ec93e086
maker2413 <maker2413@gmail.com>
2021-05-31 13:59:05 -0500

    added guide on Java loops

---
+++ Java/Loops/FizzBuzz.java
@@ -0,0 +1,37 @@
+/*
+Fizz Buzz
+Fizz Buzz is a common developer interview question; so common it’s almost cliché!
+
+The challenge is designed to weed out 99.5% programming job candidates who cannot creatively use their coding knowledge to solve coding problems.
+
+Want to give it a shot?
+
+Fizz Buzz
+Write a FizzBuzz.java program that outputs numbers from 1 to 100… with a catch:
+
+For multiples of 3, print Fizz instead of the number.
+For the multiples of 5, print Buzz.
+For numbers which are multiples of both 3 and 5, print FizzBuzz.
+*/
+
+class FizzBuzz {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i % 3 == 0) {
+        if (i % 5 == 0) {
+          System.out.println("FizzBuzz");
+        } else {
+          System.out.println("Fizz");
+        }
+      } else if (i % 5 == 0) {
+        if (i % 3 == 0) {
+          System.out.println("FizzBuzz");
+        } else {
+          System.out.println("Buzz");
+        }
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}

d71cac65e95d44a1bd9b8258f5a9ede1d26dd75f
maker2413 <maker2413@gmail.com>
2021-06-24 21:53:00 -0500

    Squashed 'Spinnaker/Spinnaker-Developer/' changes from 4e94402..32cbb61
    32cbb61 I only need to copy the kube config once :)
    d731223 testing formatting the kube config
    0d7b3c5 testing formatting the kube config
    14fb85f fixed sed logic in kube config snippet
    322cfc8 fixed namespace manifest
    5daffb2 Merge branch 'master' of github.com:/maker2413/Spinnaker-Developer
    4ac0955 setup ingress for spinnaker
    7b82b08 updated some bugs in the install script
    b4198db Merge pull request #1 from maker2413/test
    c4bb45c got each of the services running
    fbf8405 testing something
    e063e6c setting kubeconfig path in halyard config
    8c0da46 setting up minio configuration
    e038595 adding minio to spinnaker development script
    3ff02a1 moved manifests
    7162520 moved install script
    
    git-subtree-dir: Spinnaker/Spinnaker-Developer
    git-subtree-split: 32cbb61c4b5b8e2a6e07fc400d01319a689f8c76

---
+++ install_spinnaker.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+format_manifests () {
+    for f in ${BASE_DIR}/templates/*; do        
+        sed -i \
+            -e "s|NAMESPACE|${NAMESPACE}|g" \
+            -e "s|MINIO_PASSWORD|${MINIO_PASSWORD}|g" \
+            -e "s|BASE_DIR|${BASE_DIR}|g" \
+            ${f}
+    done
+}
+
+set -e
+
+BASE_DIR=/etc/spinnaker
+NAMESPACE=spinnaker
+PRIVATE_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | head -n 1)
+PUBLIC_IP=$(curl -s ifconfig.me)
+
+echo "Installing Kubernetes"
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san ${PUBLIC_IP}" sh -
+sudo kubectl config set-context --current --namespace=${NAMESPACE}
+
+echo "Installing Java"
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt update -y
+sudo apt install -y openjdk-11-jdk
+
+echo "Creating Spinnaker Directory"
+sudo mkdir -p ${BASE_DIR}/manifests
+sudo mkdir -p ${BASE_DIR}/.kube
+sudo mkdir -p ${BASE_DIR}/.hal/.secret
+sudo mkdir -p ${BASE_DIR}/.hal/default/{profiles,service-settings}
+sudo chown -R 1000 ${BASE_DIR}
+
+echo "Generate MinIO Password"
+openssl rand -base64 36 | tee ${BASE_DIR}/.hal/.secret/minio_password
+MINIO_PASSWORD=`cat ${BASE_DIR}/.hal/.secret/minio_password`
+
+echo "Copy Configs to Spinnaker Directory"
+cp -rpv manifests ${BASE_DIR}/templates/
+cp -rpv profiles ${BASE_DIR}/.hal/default/
+# cp -rpv service-settings ${BASE_DIR}/templates/
+# cp config ${BASE_DIR}/templates/
+format_manifests
+
+echo "Deploying Halyard in Kubernetes"
+kubectl apply -f ${BASE_DIR}/templates/namespace.yml
+kubectl apply -f ${BASE_DIR}/templates/halyard.yml
+
+while [[ $(kubectl get statefulset halyard -o jsonpath='{.status.readyReplicas}') -ne 1 ]];
+do
+  echo "Waiting for Halyard pod to start"
+  sleep 5;
+done
+
+echo "Creating hal Command"
+sudo tee /usr/local/bin/hal <<-'EOF'
+#!/bin/bash
+POD_NAME=$(kubectl get pod -l app=halyard -oname | cut -d'/' -f 2)
+# echo $POD_NAME
+set -x
+kubectl -n spinnaker exec -i ${POD_NAME} -- sh -c "hal $*"
+EOF
+sudo chmod 755 /usr/local/bin/hal
+
+echo "Set up Kube Config"
+cp /etc/rancher/k3s/k3s.yaml ${BASE_DIR}/.kube/config
+sudo chown -R 1000 ${BASE_DIR}
+sed -i -e "s|127.0.0.1|${PRIVATE_IP}|g" ${BASE_DIR}/.kube/config
+
+echo "Creating Endpoint file"
+if [[ ! -s ${BASE_DIR}/.hal/public_endpoint ]]; then
+  echo "Using provided public IP ${PUBLIC_IP}"
+  echo "${PUBLIC_IP}" > ${BASE_DIR}/.hal/public_endpoint
+else
+  echo "Using existing Public IP from ${BASE_DIR}/.hal/public_endpoint"
+  cat ${BASE_DIR}/.hal/public_endpoint
+fi
+
+echo "Deploy MinIO"
+kubectl apply -f ${BASE_DIR}/templates/minio.yml
+
+echo "Configuring Halyard"
+hal config provider kubernetes enable
+hal config provider kubernetes account add default
+hal config storage s3 edit --endpoint "http://minio.$NAMESPACE:9000" --bucket $NAMESPACE --access-key-id minio --secret-access-key $MINIO_PASSWORD --path-style-access true
+hal config storage edit --type s3
+hal config version edit --version 1.26.4
+hal config deploy edit --type distributed --account-name default
+
+echo "Configure URL"
+hal config security ui edit --override-base-url http://${PRIVATE_IP}:9000
+hal config security api edit --override-base-url http://${PRIVATE_IP}:8084
+
+echo "Deploy Spinnaker"
+hal deploy apply
+
+echo "Setup Ingress"
+kubectl apply -f ${BASE_DIR}/templates/spinnaker-ingress.yml

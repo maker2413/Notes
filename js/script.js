f225eb1af2e03f768df650fb867f45023f18cca5
maker2413 <maker2413@gmail.com>
2021-11-15 19:50:03 -0600

    cleaned up some of the console.log lines

--- js/script.js
+++ js/script.js
@@ -4,37 +4,27 @@ let notes = [window.location.pathname];
 // console.log(notes);
 // console.log(window.location);
 
-function createContainer() {
-}
+// function createContainer(link, html) {
+//   console.log(Number(link.dataset.level));
+// }
 
 function initializePage(note) {
   let links = Array.prototype.slice.call(note.querySelectorAll('a'));
-  // console.log(links);
 
   links.forEach(async function (el) {
     // let linkFile = el.getAttribute('href');
     let link = el.href;
-    // console.log(linkFile, link);
 
     let response = await fetch(link);
     let template = document.createElement('template');
-    console.log(response, template);
 
     template.innerHTML = await response.text();
-    console.log(template);
+    console.log(template.content.querySelector('.node').outerHTML);
 
-    createContainer(el, );
-
-    // async function find() {
-    //   let response = await fetch(link);
-    //   console.log(response);
-    // }
-    // return find();
+    createContainer(el, template.content.querySelector('.node').outerHTML);
   })
 }
 
 window.onload = function() {
-  // console.log(document.querySelector(".note"));
-
   initializePage(document.querySelector('.note'));
 };

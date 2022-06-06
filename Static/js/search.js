cde5bcf707082ab27f1a93c6d60825b05de36ef9
maker2413 <maker2413@gmail.com>
2022-06-05 23:01:16 -0500

    updated favicon

--- Static/js/search.js
+++ Static/js/search.js
@@ -9,5 +9,8 @@ window.onload = function() {
   // Perform a search if there is a query
   if (query) {
     document.getElementById("search-input").setAttribute("value", query);
+
+    console.log(window);
+    console.log(window.store);
   }
 };

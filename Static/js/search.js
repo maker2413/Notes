f96bfdf1f6683ed510ab765e4c98275af93610a7
maker2413 <maker2413@gmail.com>
2022-06-23 22:45:40 -0500

    got search bar working!

--- Static/js/search.js
+++ Static/js/search.js
@@ -1,16 +1,90 @@
-window.onload = function() {
-  // Get the query
-  const query = new URLSearchParams(window.location.search).get("query");
+"use strict";
 
-  console.log(query);
-  console.log(document);
-  console.log(document.getElementById('search-input'));
+var LUNR_CONFIG = {
+  "resultsElementId": "searchResults",  // Element to contain results
+  "countElementId": "resultCount"  // Element showing number of results
+};
 
-  // Perform a search if there is a query
-  if (query) {
-    document.getElementById("search-input").setAttribute("value", query);
+// Get URL arguments
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
-    console.log(window);
-    console.log(window.store);
+// Parse search results into HTML
+function parseLunrResults(results) {
+  var html = [];
+  for (var i = 0; i < results.length; i++) {
+    var id = results[i]["ref"];
+    var item = PREVIEW_LOOKUP[id];
+    var title = item["t"];
+    var preview = item["p"];
+    var link = item["l"];
+    var result = ('<p><span class="result-title"><a href="' + link + '">'
+                  + title + '</a></span><br><span class="result-preview">'
+                  + preview + '</span></p>');
+    html.push(result);
+  };
+  if (html.length) {
+    return html.join("");
   }
+  else {
+    return "<p>Your search returned no results.</p>";
+  }
+};
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+};
+
+function showResultCount(query, total, domElementId) {
+  if (total == 0) {
+    return;
+  }
+
+  var s = "";
+  if (total > 1) {
+    s = "s";
+  };
+  var found = "<p>Found " + total + " result" + s;
+  if (query != "" && query != null) {
+    query = escapeHtml(query);
+    var forQuery = ' for <span class="result-query">' + query + '</span>';
+  }
+  else {
+    var forQuery = "";
+  };
+  var element = document.getElementById(domElementId);
+  element.innerHTML = found + forQuery + "</p>";
+};
+
+function searchLunr(query) {
+  var idx = lunr.Index.load(LUNR_DATA);
+  // Write results to page
+  var results = idx.search(query);
+  var resultHtml = parseLunrResults(results);
+  var elementId = LUNR_CONFIG["resultsElementId"];
+  document.getElementById(elementId).innerHTML = resultHtml;
+
+  var count = results.length;
+  showResultCount(query, count, LUNR_CONFIG["countElementId"]);
+};
+
+// Read query parameters and perform search
+// Get the query
+const query = new URLSearchParams(window.location.search).get("query");
+
+if (query != "" && query != null) {
+  document.getElementById("search-input").setAttribute("value", query);
+  searchLunr(query);
 };
